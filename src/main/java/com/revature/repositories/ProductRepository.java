86cf2951a59b7011807bffef1ad5646b88e6d87f
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-08-01 15:53:20 -0700

    fuzzy string search for product name

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -1,7 +1,22 @@
 package com.revature.repositories;
 
-import com.revature.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.Product;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+	//returns a list of product names whose distance between the user input and the product name in db is less than half the length of the productname
+	//uses levenshtein's function
+	@Query(value="SELECT name FROM products WHERE levenshtein(name, ?1) <= (LENGTH(name))/2"
+			+ "ORDER BY levenshtein(name, ?1)", nativeQuery=true)
+	public List<Product> findByName(String name);
+	
+	
+	
 }
