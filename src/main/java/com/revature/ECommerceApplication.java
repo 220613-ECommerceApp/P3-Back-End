b5c2e6fc16c13b46805c864704132e5c7ca1f27d
saml5 <samlerch5@gmail.com>
2022-08-01 15:59:03 -0400

    test commit

--- src/main/java/com/revature/ECommerceApplication.java
+++ src/main/java/com/revature/ECommerceApplication.java
@@ -2,7 +2,7 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Test comment
 @SpringBootApplication
 public class ECommerceApplication {
 
