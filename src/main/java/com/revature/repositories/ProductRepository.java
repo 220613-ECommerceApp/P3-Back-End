d267d41bffeb70fe948416db3a57023387958633
GoodCodeSeeker <ka125@revature.net>
2022-08-02 10:54:56 -0400

    Added search by price and search by price range

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -1,7 +1,25 @@
 package com.revature.repositories;
 
-import com.revature.models.Product;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.revature.models.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	@Query(value="SELECT name FROM products WHERE DIFFERENCE(name, ?1) >2"
+			+ "ORDER BY DIFFERENCE(name, ?1)", nativeQuery=true)
+	public List<Product> findBySimilarName(String name);
+	
+	@Query("SELECT p FROM products p WHERE CONCAT(p.name) LIKE %?1%")
+	public List<Product> search(String keyword);
+	
+	@Query("SELECT p FROM products p WHERE p.price BETWEEN startPrice AND endPrice")
+	public List<Product> priceRangeSearch(double startPrice, double endPrice);
+	
 }
+
+
+
