e18876aa12e2828b2c241a5c7314218d7fc3fa2f
formula38 <louismconley@gmail.com>
2022-08-02 14:37:16 -0700

    Updates to WishListItemRepository

--- src/main/java/com/revature/repositories/WishListItemRepository.java
+++ src/main/java/com/revature/repositories/WishListItemRepository.java
@@ -1,8 +1,29 @@
 package com.revature.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 import com.revature.models.WishListItem;
 
 public interface WishListItemRepository extends JpaRepository<WishListItem, Integer>{
     
+    Optional<WishListItem> findByUserId(int userId);
+
+    // custom query to change quantity of an item inside the wish list
+    @Modifying
+    @Query
+    (value = "UPDATE wishlist_item w set w.quantity = ? WHERE w.id = ? AND w.product_id = ? AND w.user_id = ?", nativeQuery = true)
+    void updateQuantityInWishList(Integer quantity, Integer wishListId, Integer productId, Integer userId);
+
+    @Modifying
+    @Query
+    (value = "DELETE FROM wishlist_item w WHERE w.id = ? AND w.product_id = ? AND w.user_id = ?", nativeQuery = true)
+    void deleteProductFromWishList(Integer wishListId, Integer productId, Integer userId);
+
+    WishListItem getById(int id);
+
 }
+
