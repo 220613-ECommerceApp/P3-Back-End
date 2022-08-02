2f17a9fcddeb49a8febb1b2a382e7a9adf765d40
formula38 <louismconley@gmail.com>
2022-08-02 14:26:28 -0700

    Updates to WishListItemService

--- src/main/java/com/revature/models/WishListItem.java
+++ src/main/java/com/revature/models/WishListItem.java
@@ -1,16 +1,11 @@
 package com.revature.models;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.ManyToAny;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/revature/services/WishListItemService.java
+++ src/main/java/com/revature/services/WishListItemService.java
@@ -3,39 +3,53 @@ package com.revature.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.dtos.ProductInfo;
 import com.revature.models.WishListItem;
+import com.revature.repositories.UserRepository;
 import com.revature.repositories.WishListItemRepository;
 
 @Service
 public class WishListItemService {
 
+    @Autowired
     private final WishListItemRepository wishListItemRepository;
+
+    @Autowired
+    private final UserRepository userRepository;
     
-    public WishListItemService(WishListItemRepository wishListItemRepository){
+    public WishListItemService(WishListItemRepository wishListItemRepository, UserRepository userRepository){
         this.wishListItemRepository = wishListItemRepository;
+        this.userRepository = userRepository;
     }
 
-    public List<WishListItem> findAllWishListItem(){
-        return wishListItemRepository.findAll();
+    public WishListItem createWishList(WishListItem wishListItem, int userId){
+        WishListItem wLi = new WishListItem();
+        wLi.setUserId(userId);
+        return wishListItemRepository.save(wLi);
     }
 
-    public Optional<WishListItem> findWishListItemById(int id){
-        return wishListItemRepository.findById(id);
+    public WishListItem addToWishList(int productId, int userId, int quantity){
+        WishListItem wLi = wishListItemRepository.findByuserId(userId).get();
+        wLi.setProductId(productId);
+        wishListItemRepository.updateQuantityInCart(quantity, wLi.getId(), productId, wLi.getUserId());
+        return wLi;
     }
 
-    public WishListItem saveWishListItem(WishListItem wishListItem){
-        return wishListItemRepository.save(wishListItem);
+    public WishListItem removeWishListItem(Integer wishListId, Integer productId, Integer userId){
+        WishListItem wLi = wishListItemRepository.getById(wishListId);
+        wishListItemRepository.deleteProductFromWishList(wishListId, productId, userId);
+        return wLi;
     }
 
-    public List<WishListItem> saveWishList(List<WishListItem> wishList, List<ProductInfo> metadata){
-        return wishListItemRepository.saveAll(wishList);
-    }
-
-    public void deleteWishListItem(int id){
-        wishListItemRepository.deleteById(id);
+    public WishListItem updateItemQuantity(int quantity, int wishListId, int productId, int userId){
+        WishListItem wLi = wishListItemRepository.getById(wishListId);
+        wishListItemRepository.updateQuantityInWishList(quantity, wishListId, productId, userId);
+        return wLi;
     }
 
 }
+
+
