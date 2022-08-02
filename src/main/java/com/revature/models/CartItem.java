c41f61932dfdb7f25204c68be1452a297d20217d
ali alrubaye <alialrubaye1990@hotmail.com>
2022-08-02 14:53:41 -0400

    added names to fields and the table of cartitem model

--- src/main/java/com/revature/models/CartItem.java
+++ src/main/java/com/revature/models/CartItem.java
@@ -1,5 +1,6 @@
 package com.revature.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
@@ -8,23 +9,32 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "cart_item")
 public class CartItem
 {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 	private int quantity;
+	
+	
 	@ManyToOne(targetEntity = Product.class)
+	@JoinColumn(name = "product_id")
 	private int productId;
+	
+	
 	@ManyToOne(targetEntity = User.class)
+	@JoinColumn(name = "user_id")
 	private int userId;
 }
