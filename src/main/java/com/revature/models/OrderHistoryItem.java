685e37b9cfe57f9e55fc1154f9ea6e88c70869b1
Anthony Lopez <28433525+tlb99@users.noreply.github.com>
2022-08-02 12:37:06 -0700

    created model file

---
+++ src/main/java/com/revature/models/OrderHistoryItem.java
@@ -0,0 +1,44 @@
+package com.revature.models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="orderhistory_item")
+public class OrderHistoryItem {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+	@ManyToOne(targetEntity = Product.class)
+    @JoinColumn(name="product_id")
+	private int productId;
+
+	@ManyToOne(targetEntity = User.class)
+    @JoinColumn(name="user_id")
+	private int userId;
+
+    @Column(name="quantity")
+	private int quantity;
+
+    @Column(name="purchase_time", insertable=false)
+    private Timestamp purchaseTime;
+}
