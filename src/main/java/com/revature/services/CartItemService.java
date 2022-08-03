1f7c71b15c083c115d1431f1f9d9e6cdfbaee2bd
ali alrubaye <alialrubaye1990@hotmail.com>
2022-08-03 11:36:10 -0400

    added get all cartItems by userId in cartItemService

--- src/main/java/com/revature/services/CartItemService.java
+++ src/main/java/com/revature/services/CartItemService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -51,6 +53,10 @@ public class CartItemService {
 		 
 	}
 	
+	public List<CartItem> getByUserId(int id){
+		
+		return cartItemRepository.findListByUserId(id);
+	}
 	
  
 
