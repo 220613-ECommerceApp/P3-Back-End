7d8ef391613c200d3fb21cc9d66e56477467ff66
ali alrubaye <alialrubaye1990@hotmail.com>
2022-08-03 11:16:39 -0400

    done with removeFromCart

--- src/main/java/com/revature/controllers/CartItemController.java
+++ src/main/java/com/revature/controllers/CartItemController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,15 @@ public class CartItemController {
 		CartItem ci = cis.addItemToCart(productid, userid, quantity);
 		return new ResponseEntity<CartItem>(ci, HttpStatus.OK);
 	}
+	
+	//All counts of a single item
+	@PostMapping("/removefromcart/{productid}")
+	public ResponseEntity<CartItem> removeFromCart(@PathVariable("productid") Integer productid, @RequestBody int userid) {
+		CartItem ci = cis.removeItem(productid, userid);
+		return new ResponseEntity<CartItem>(ci, HttpStatus.OK);
+	}
+	
+	
 	
 	
 	
