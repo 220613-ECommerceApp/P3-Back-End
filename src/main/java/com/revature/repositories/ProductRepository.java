0e1de3b96743e69fcbeb65437e5b53d8ba64fd05
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-08-04 08:29:15 -0700

    reduced search algorithm complexity + deleted ProductRepository comments

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -9,24 +9,7 @@ import com.revature.models.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-      /*
-       * fuzzy string search
-       * levenshtein function might need to be installed as an extention
-       * returns a list of product names whose distance between the user input and the
-       * product name in db is less than half the length of the productname
-       * uses levenshtein's function
-       */
-      // @Query(value="SELECT name FROM products WHERE levenshtein(name, ?1) <=
-      // (LENGTH(name))/2"
-      // + "ORDER BY levenshtein(name, ?1)", nativeQuery=true)
-      // public List<Product> findBySimilarName(String name)
-
-      /*
-       * fuzzy string search
-       * another alternative to levenshtein + didnt need to install
-       * returns a list of products where the similarity between input and db name is
-       * >2 based on the SOUNDEX function
-       */
+     
       @Query("FROM Product WHERE lower(name) LIKE lower(concat('%', ?1, '%'))")
       public List<Product> findBySimilarName(String pattern);
 
