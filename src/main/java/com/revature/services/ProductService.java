3a93efb2bc00910915377cf8c8eb11b94fd992b1
xXstrawcakeXx <107396089+xXstrawcakeXx@users.noreply.github.com>
2022-08-05 07:49:18 -0700

    deleted unused imports in product test/service/controller

--- src/main/java/com/revature/services/ProductService.java
+++ src/main/java/com/revature/services/ProductService.java
@@ -3,7 +3,6 @@ package com.revature.services;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -11,11 +10,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import com.revature.dtos.ProductInfo;
 import com.revature.models.Product;
-import com.revature.models.ProductTagJunction;
 import com.revature.repositories.ProductRepository;
 
 @Service
--- src/test/java/com/revature/services/ProductServiceTest.java
+++ src/test/java/com/revature/services/ProductServiceTest.java
@@ -1,18 +1,15 @@
 package com.revature.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
