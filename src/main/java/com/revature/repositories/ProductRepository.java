b69045cfb2bd68380781f133b23dcd1de47851a4
GoodCodeSeeker <ka125@revature.net>
2022-08-05 12:11:05 -0400

    Updated

--- src/main/java/com/revature/repositories/ProductRepository.java
+++ src/main/java/com/revature/repositories/ProductRepository.java
@@ -1,33 +1,17 @@
 package com.revature.repositories;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import com.revature.models.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	/*
-	 * fuzzy string search
-	 * levenshtein function might need to be installed as an extention
-	 * returns a list of product names whose distance between the user input and the
-	 * product name in db is less than half the length of the productname
-	 * uses levenshtein's function
-	 */
-	// @Query(value="SELECT name FROM products WHERE levenshtein(name, ?1) <=
-	// (LENGTH(name))/2"
-	// + "ORDER BY levenshtein(name, ?1)", nativeQuery=true)
-	// public List<Product> findBySimilarName(String name)
-
-	/*
-	 * fuzzy string search
-	 * another alternative to levenshtein + didnt need to install
-	 * returns a list of products where the similarity between input and db name is
-	 * >2 based on the SOUNDEX function
-	 */
       @Query(value = "select * from products where lower(name) ~* :pattern", nativeQuery = true)
       public List<Product> findBySimilarName(@Param("pattern") String pattern);
 
