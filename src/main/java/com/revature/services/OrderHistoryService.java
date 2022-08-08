19a76896ab3196dca68e28d3f5490af498a41bc7
Anthony Lopez <28433525+tlb99@users.noreply.github.com>
2022-08-08 09:24:27 -0700

    added OrderInfo, reworked OrderHistoryService

--- src/main/java/com/revature/services/OrderHistoryService.java
+++ src/main/java/com/revature/services/OrderHistoryService.java
@@ -1,5 +1,6 @@
 package com.revature.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -7,6 +8,8 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import com.revature.repositories.OrderHistoryItemRepository;
+import com.revature.dtos.OrderInfo;
+import com.revature.dtos.ProductInfo;
 import com.revature.models.OrderHistoryItem;
 
 @Service
@@ -17,13 +20,35 @@ public class OrderHistoryService {
         this.orderHistoryItemRepository = orderHistoryItemRepository;
     }
     
-    public List<OrderHistoryItem> findByUserId(int id){
-        return orderHistoryItemRepository.findByUserId(id);
+    public List<List<OrderInfo>> findByUserId(int id){
+        List<OrderHistoryItem> orders = orderHistoryItemRepository.findByUserId(id);
+        List<List<OrderInfo>> list = new ArrayList<>();
+
+        list.add(new ArrayList<OrderInfo>());
+        list.get(0).add(convertToDto(orders.get(0)));
+
+        for(int i = 1; i < orders.size(); ++i){
+            if(!orders.get(i - 1).getPurchaseTime().equals(orders.get(i).getPurchaseTime())){
+                list.add(new ArrayList<OrderInfo>());
+            }
+            list.get(list.size()-1).add(convertToDto(orders.get(i)));
+        }
+
+        return list;
     }
 
     @Transactional
     public void addToOrderHistory(List<OrderHistoryItem> orderInfoList){
         for(OrderHistoryItem o : orderInfoList)
             orderHistoryItemRepository.addToOrderHistory(o.getProduct().getId(), o.getUser().getId(), o.getQuantity());
+    }
+
+    private OrderInfo convertToDto(OrderHistoryItem orderHistoryItem){
+        return new OrderInfo(
+            orderHistoryItem.getId(),
+            orderHistoryItem.getProduct(),
+            orderHistoryItem.getQuantity(),
+            orderHistoryItem.getPurchaseTime()
+            );
     }
 }
