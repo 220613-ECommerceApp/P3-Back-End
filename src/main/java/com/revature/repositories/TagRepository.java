de97a7e8fef7d0d03758305baae146ca065d0e3c
Yuriy Koziy <yukoziy@gmail.com>
2022-08-08 10:39:52 -0400

    Comment out @Query

--- src/main/java/com/revature/repositories/TagRepository.java
+++ src/main/java/com/revature/repositories/TagRepository.java
@@ -13,7 +13,7 @@ import com.revature.models.Tag;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, String> {
 
-      @Query(value = "SELECT * FROM products WHERE id IN "
-                  + "(SELECT product_id FROM tag_junction WHERE tag_name = :tagName)", nativeQuery = true)
+     // @Query(value = "SELECT * FROM products WHERE id IN "
+   //               + "(SELECT product_id FROM tag_junction WHERE tag_name = :tagName)", nativeQuery = true)
       // public List<Product> tagSearch(@Param("tagName") String tagName);
 }
