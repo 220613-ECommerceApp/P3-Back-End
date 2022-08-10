b42d7cf3e0a6148ff177084f967c401f9b694350
Yuriy Koziy <yukoziy@gmail.com>
2022-08-10 06:56:01 -0400

    fix code smells

--- src/main/java/com/revature/controllers/TagController.java
+++ src/main/java/com/revature/controllers/TagController.java
@@ -1,25 +1,15 @@
 package com.revature.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.Tag;
 import com.revature.services.TagService;
-import com.revature.annotations.Authorized;
 
 @RestController
 @RequestMapping("/api/tag")
