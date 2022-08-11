8108a5a719ab1e3e1b7ce90370402813d430f85c
Anthony Lopez <28433525+tlb99@users.noreply.github.com>
2022-08-11 07:58:05 -0700

    changed construction injection to autowired injection in services and controllers

--- src/main/java/com/revature/services/WishListItemService.java
+++ src/main/java/com/revature/services/WishListItemService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.models.Product;
@@ -14,12 +15,8 @@ import com.revature.repositories.WishListItemRepository;
 @Service
 public class WishListItemService {
 
-    private final WishListItemRepository wishListItemRepository;
-
-    
-    public WishListItemService(WishListItemRepository wishListItemRepository){
-        this.wishListItemRepository = wishListItemRepository;
-    }
+    @Autowired
+    private WishListItemRepository wishListItemRepository;
 
     public WishListItem createWishList(Product product, User user){
         WishListItem wLi = new WishListItem();
