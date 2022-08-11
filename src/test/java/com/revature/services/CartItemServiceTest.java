1567ac1d41a66e75bac68fa87cebabd909c47e58
okraus22 <odk22@yahoo.com>
2022-08-11 16:48:46 -0400

    Finished cart service tests

--- src/test/java/com/revature/services/CartItemServiceTest.java
+++ src/test/java/com/revature/services/CartItemServiceTest.java
@@ -1,6 +1,8 @@
 package com.revature.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -63,4 +65,54 @@ public class CartItemServiceTest
 		assertEquals(returnedList, dummyList);
 		
 	}
+	
+	@Test
+	public void TestAddItem()
+	{
+		when(mockCartRepo.addToCart(1, 1, 2)).thenReturn(true);
+		when(mockCartRepo.findByUserIdAndProductId(1, 1)).thenReturn(dummyItem1);
+		CartItem returnedItem = cartService.addItemToCart(1, 1, 2);
+		assertEquals(returnedItem, dummyItem1);
+	}
+	
+	@Test
+	public void TestRemoveItem()
+	{
+		when(mockCartRepo.deleteProductFromCart(1, 1)).thenReturn(true);
+		when(mockCartRepo.findByUserIdAndProductId(1, 1)).thenReturn(null);
+		CartItem returnedItem = cartService.removeItem(1, 1);
+		assertNull(returnedItem);
+	}
+	
+	@Test
+	public void TestUpdateQunatity()
+	{
+		when(mockCartRepo.updateQuantityInCart(2, 1, 1)).thenReturn(true);
+		when(mockCartRepo.findByUserIdAndProductId(1, 1)).thenReturn(dummyItem1);
+		CartItem returnedItem = cartService.updateItemQuantity(2, 1, 1);
+		assertEquals(returnedItem, dummyItem1);
+	}
+	
+	
+	@Test
+	public void TestClearCart()
+	{
+		List<CartItem> emptyList = new ArrayList<CartItem>();
+		when(mockCartRepo.clearCartForUser(1)).thenReturn(true);
+		when(mockCartRepo.findListByUserId(1)).thenReturn(emptyList);
+		List<CartItem> returnedList = cartService.clearCart(1);
+		assertEquals(returnedList, emptyList);
+	}
+	
+	@Test
+	public void TestCreateCart()
+	{
+		when(mockUserRepo.findById(1)).thenReturn(dummyUser);
+		CartItem ci = new CartItem();
+		ci.setUser(dummyUser);
+		when(mockCartRepo.save(ci)).thenReturn(dummyItem1);
+		CartItem returnedItem = cartService.createCart(1);
+		assertEquals(returnedItem, dummyItem1);
+	}
+	
 }
