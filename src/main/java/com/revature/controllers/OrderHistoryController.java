578f879be93b9c4b843e930743e62f20b686635f
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-08-13 14:13:26 -0400

    removing unused imports

--- src/main/java/com/revature/controllers/OrderHistoryController.java
+++ src/main/java/com/revature/controllers/OrderHistoryController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
