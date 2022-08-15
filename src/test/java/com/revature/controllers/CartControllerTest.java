fe1a1693baed3f7ca9cbe5053a7a909ecf2d47db
okraus22 <odk22@yahoo.com>
2022-08-15 10:33:23 -0400

    back end controller testing for cart

--- src/test/java/com/revature/controllers/CartControllerTest.java
+++ src/test/java/com/revature/controllers/CartControllerTest.java
@@ -1,13 +1,24 @@
 package com.revature.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.CartItem;
+import com.revature.models.Product;
+import com.revature.utils.JWTUtil;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,5 +35,53 @@ public class CartControllerTest
 
     private String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20iLCJ1c2VybmFtZSI6InRlc3QifQ.6l6ywOw-LNeYfJ0cDyK2lN1YulkFLbTmlOazm2sjAQQ";
     
+    /*@Test
+    void TestAddToCart() throws Exception
+    {
+    	//System.out.println("is this visible");
+    	
+    	 MvcResult result = mockMvc.perform(post("/api/cart/addtocart/{productid}", "1")
+    			 .header("authorization", "Bearer " + token)
+    			 .header("quantity", 2))
+    			 .andExpect(status().isOk())
+                 .andReturn();
+    	 
+    	 //String json = result.getResponse().getContentAsString();
+    	 //System.out.println("////////////\nthe json output is: "+json);
+    			 
+    }/**/
     
+    @Test
+    void TestRemoveFromCart() throws Exception
+    {
+    	//System.out.println("is this visible");
+    	
+    	 MvcResult result = mockMvc.perform(delete("/api/cart/removefromcart/{productid}", "1")
+    			 .header("authorization", "Bearer " + token))
+    			 .andExpect(status().isOk())
+                 .andReturn();
+    	 
+    	 //String json = result.getResponse().getContentAsString();
+    	 //CartItem item = mapper.readValue(json, CartItem.class);
+    	 //assertNull(item);
+    			 
+    }
+    
+   /*@Test
+    void TestUpdateCart() throws Exception
+    {
+    	//System.out.println("is this visible");
+	   //int userid = JWTUtil.verifyUserToken(" "+token);
+	   //System.out.println("////////////////\nthe token refers to user: "+ userid);
+    	 MvcResult result = mockMvc.perform(put("/api/cart/updatecart/{productid}}", "1")
+    			 .header("authorization", "Bearer " + token)
+    			 .header("quantity", 3))
+    			 .andExpect(status().isOk())
+                 .andReturn();
+    	 
+    	 //String json = result.getResponse().getContentAsString();
+    	 //CartItem item = mapper.readValue(json, CartItem.class);
+    	 //assertNull(item);
+    			 
+    }/**/
 }
